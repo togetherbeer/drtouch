3b67c65cc403129817b1602f5fb39a846d3522b8
mooff <mooff@together.beer>
2021-07-11 09:31:20 +0100

    Small performance adjustment - eliminating some method calls

--- drtouch.rb
+++ drtouch.rb
@@ -5,63 +5,63 @@
 # DRTouch class, Handles the API implementation of drtouch library!
 class DRTouch
   def init
-    $gtk.args.state.drtouch.emulated         ||= !mobile ? 1 : 0
-    $gtk.args.state.drtouch.current_touches  ||= [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-    $gtk.args.state.drtouch.touches_pos      ||= []
-    $gtk.args.state.drtouch.previous_touches ||= [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    $state.drtouch.emulated         ||= desktop? ? 1 : 0
+    $state.drtouch.current_touches  ||= [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    $state.drtouch.touches_pos      ||= []
+    $state.drtouch.previous_touches ||= [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
   end
 
   def update
-    $gtk.args.state.drtouch.previous_touches = $gtk.args.state.drtouch.current_touches
+    $state.drtouch.previous_touches = $state.drtouch.current_touches
     
-    if ($gtk.args.state.drtouch.emulated == 0)
+    if ($state.drtouch.emulated == 0)
       count = 0
   
-      $gtk.args.inputs.touch.each { |k, v|
-        $gtk.args.state.drtouch.current_touches[count] = 1
-        $gtk.args.state.drtouch.touches_pos[count] = { x: v.x, y: v.y }
+      $gtk.inputs.touch.each { |k, v|
+        $state.drtouch.current_touches[count] = 1
+        $state.drtouch.touches_pos[count] = { x: v.x, y: v.y }
         count += 1
       }
     
       (10 - count).times.map do |i|
-        $gtk.args.state.drtouch.current_touches[count + i] = 0
-        $gtk.args.state.drtouch.touches_pos[count + i] = { x: -1, y: -1 }
+        $state.drtouch.current_touches[count + i] = 0
+        $state.drtouch.touches_pos[count + i] = { x: -1, y: -1 }
       end
     else
-      $gtk.args.state.drtouch.current_touches[0] = $gtk.args.inputs.mouse.button_left ? 1 : 0
-      $gtk.args.state.drtouch.touches_pos[0] = { x: $gtk.args.inputs.mouse.x, y: $gtk.args.inputs.mouse.y }
+      $state.drtouch.current_touches[0] = $gtk.inputs.mouse.button_left ? 1 : 0
+      $state.drtouch.touches_pos[0] = { x: $gtk.inputs.mouse.x, y: $gtk.inputs.mouse.y }
       
       9.times.map do |i|
-        $gtk.args.state.drtouch.current_touches[1 + i] = 0
-        $gtk.args.state.drtouch.touches_pos[1 + i] = { x: -1, y: -1 }
+        $state.drtouch.current_touches[1 + i] = 0
+        $state.drtouch.touches_pos[1 + i] = { x: -1, y: -1 }
       end
     end
   end
   
   def down(i)
-    return ($gtk.args.state.drtouch.current_touches[i] == 1)
+    return ($state.drtouch.current_touches[i] == 1)
   end
   
   def up(i)
-    return ($gtk.args.state.drtouch.current_touches[i] == 0)
+    return ($state.drtouch.current_touches[i] == 0)
   end
   
   def tap(i)
-    if $gtk.args.state.drtouch.emulated == 0
-      return ($gtk.args.state.drtouch.current_touches[i] == 1 && $gtk.args.state.drtouch.previous_touches[i] == 0)
+    if $state.drtouch.emulated == 0
+      return ($state.drtouch.current_touches[i] == 1 && $state.drtouch.previous_touches[i] == 0)
     else
       if (i == 0)
-        return $gtk.args.inputs.mouse.click
+        return $gtk.inputs.mouse.click
       end
     end
   end
   
   def release(i)
-    if $gtk.args.state.drtouch.emulated == 0
-      return ($gtk.args.state.drtouch.current_touches[i] == 0 && $gtk.args.state.drtouch.previous_touches[i] == 0)
+    if $state.drtouch.emulated == 0
+      return ($state.drtouch.current_touches[i] == 0 && $state.drtouch.previous_touches[i] == 0)
     else
       if (i == 0)
-        return !$gtk.args.inputs.mouse.click
+        return !$gtk.inputs.mouse.click
       end
     end
   end
@@ -69,8 +69,8 @@ class DRTouch
   def touches
     count = 0
     
-    $gtk.args.state.drtouch.current_touches.length.times.map do |t|
-      if $gtk.args.state.drtouch.current_touches[t] == 1
+    $state.drtouch.current_touches.length.times.map do |t|
+      if $state.drtouch.current_touches[t] == 1
         count += 1
       end
     end
@@ -79,7 +79,7 @@ class DRTouch
   end
   
   def pos(i)
-    t = $gtk.args.state.drtouch.touches_pos[i]
+    t = $state.drtouch.touches_pos[i]
     
     if t
       return { x: t.x, y: t.y }
@@ -103,7 +103,11 @@ class DRTouch
     return ({ x: p.x, y: p.y, w: 1, h: 1 }).intersect_rect?(r)
   end
   
-  def mobile
+  def mobile?
     return ($gtk.platform == "iOS" || $gtk.platform == "Android")
+  end
+
+  def desktop?
+    return not mobile?
   end
 end
